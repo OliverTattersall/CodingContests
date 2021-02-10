7fb6997ce6eebbb6bcad01c63a0a6b8006bf3950
OliverTattersall <oliver.tattersall@ucc.on.ca>
2021-02-10 09:57:20 -0500

    update

--- CCC/CCC practice 2019/S4.py
+++ CCC/CCC practice 2019/S4.py
@@ -3,52 +3,27 @@ attractions=input().split(" ")
 totalattractions=int(totalattractions)
 maxattractions=int(maxattractions)
 offdays=totalattractions%maxattractions
+for i in range(totalattractions):
+    attractions[i] = int(attractions[i])
 
 
-def go(list1,list2, numperday):
-    deleted=[]
-    day=[]
-    sum1=[]
-    sum2=[]
-    for i in range(offdays):
-        deleted.append(list1[0])
-        list1.pop(0)
-    #print(list2)
-    for i in range(0,len(list1),numperday):
-        day=[int(list2[i]),int(list2[i+1]),int(list2[i+2])]
-        sum1.append(max(day))
-    sum1.append(int(max(deleted)))
-    #print(list2)
-    #print(offdays)
-    for i in range(offdays):
-        list2.insert(0, deleted[i])
-    deleted=[]
-    for i in range(offdays):
-        deleted.append(list2[-1])
-        list2.pop(-1)
-    #print(list2)
-    for i in range(0,len(list2), numperday):
-        day=[int(list2[i]),int(list2[i+1]),int(list2[i+2])]
-        sum2.append(max(day))
-    sum2.append(int(max(deleted)))
-    if sum(sum1)>=sum(sum2):
-        scores.append(sum(sum1))
-    elif sum(sum2)>sum(sum1):
-        scores.append(sum(sum2))
+def solve(ats, off):
+    # print(ats, off)
+    if len(ats)==maxattractions or len(ats)==offdays:
+        return max(ats)
     
+    incl =max(ats[0:maxattractions])+solve(ats[maxattractions:], off)
+    if off:
+        offday = float("-inf")
+    else:
+        offday = max(ats[0:offdays]) + solve(ats[offdays:], True)
 
+    print(incl, offday)
+    return max(incl, offday)
 
-scores=[]
-#print(attractions)
 if offdays==0:
-    sumlist=[]
-    for i in range(0,totalattractions,maxattractions):
-        day=[int(attractions[i]),int(attractions[i+1]),int(attractions[i+2])]
-        sumlist.append(max(day))
-    
-    scores.append(sum(sumlist))
+    answer = solve(attractions, True)
 else:
-    go(attractions, attractions, maxattractions)
+    answer = solve(attractions, False)
 
-
-print(scores[0])
+print(answer)
--- ECOO/ECOO2019practice/R1P2.py
+++ ECOO/ECOO2019practice/R1P2.py
@@ -1,47 +1,91 @@
+#loops through all 10 test cases
+for _ in range(1):
 
-for _ in range(10):
     first = input().split()
+
+    #number of rules to come
     r = int(first[0])
+
+    #number of iterations the program will do
     t = int(first[1])
+
+    #creating a dictionary to hold how many of each letter each rule has. will be two dimensional dictionary
     rules = {}
+    #creating a dictionary to hold each rule with the key as the letter trigger
     rules2={}
+
+    #the original string that gets transformed
     original = first[2]
+
+    #loops through to get the rest of the inputs
     for i in range(r):
         line = input().split()
+        #temporary dictionary which will contain the letter count for each letter in the current rul
         temp = {}
+
+        #for loop to count letters in the rule
         for i in range(len(line[1])):
             temp[line[1][i]] = temp.get(line[1][i], 0)+1
+        
+        #adds dictionary to rules paired with the letter that triggers this rule
         rules[line[0]] = temp
+        #adds the string to rules2 paired with the letter that triggers this rule
         rules2[line[0]] = line[1]
-    # print(rules, rules2)
+
+
+    #first letter. this variable will keep track of the first letter through each iteration
     front = original[0]
+
+    #last letter. this variable will keep track of the last letter through each iteration
     end = original[-1]
 
+    #dictionary for how many letters in the string
     letters={}
 
+    #loops through original string to count the letters using a dictionary
     for i in range(len(original)):
         letters[original[i]] = letters.get(original[i], 0)+1
 
     # print(letters)
+
+    #iterates t times
     for i in range(t):
+
+        #applies the rule for the first and last letter, and take the first and last letter of the result
         front = rules2[front][0]
         end = rules2[end][-1]
-        keys = list(letters.keys())
-        # print(letters)
-        count={}
-        for j in keys:
-            keys2 = list(rules[j].keys())
-            # print(j, keys2, rules)
 
+        #creates list of the keys in the letters dictionary.
+        keys = list(letters.keys())
+        
+        #a temporary dictionary to count how many letters will be created in this iteration
+        count={}
+
+        #loops through each item in keys
+        for key in keys:
+
+            #creates a new list of the keys in the child dictionary for each letter 
+            # 
+            #
+            keys2 = list(rules[key].keys())
+
+            #loops through each key in keys2 
             for k in keys2:
-                # print()
-                count[k] = count.get(k, 0)+(rules[j][k])*letters[j]
-        # print(count)
+                
+                #updates the key dictionary 
+                count[k] = count.get(k, 0)+(rules[key][k])*letters[key]
+        
+
+        #letters becomes the count dictionary. Uses copy() to not create a reference to the count dictionary so that when the count dictionary is set to {}, the letters dictionary isn't.
         letters = count.copy()
 
+
+    #sum variable to count how many letters
     sum = 0
-    # print(keys)
-    for i in range(len(keys)):
-        sum+=letters[keys[i]]
-    # print(letters, sum)
+    
+    #loops through each key in the final key list and adds the value connected to that pair to the sum 
+    for key in keys:
+        sum+=letters[key]
+   
+    #prints the first and last letter, and the number of letters
     print(front+end, sum)
