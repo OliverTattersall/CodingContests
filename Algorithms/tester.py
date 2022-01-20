1f811bb0286edee580672d5ef61aa3d7eae14623
Oliver Tattersall <oliver.tattersall@olivertattersall.local>
2022-01-20 15:11:16 -0500

    update

--- Algorithms/tester.py
+++ Algorithms/tester.py
@@ -1,7 +1,19 @@
-import random
 
 
-x=(3,4)
-print(x)
-x=x+(5,)
-print(x)
+x = "ABCBDAB"
+y = "BDCABA"
+
+def findLCS(x,y):
+    T = [[0 for _ in range(len(x)+1)] for _ in range(len(y)+1)]
+    
+    for i in range(1, len(y)+1):
+        for j in range(1, len(x)+1):
+            if x[j-1] == y[i-1]:
+                T[i][j] = T[i-1][j-1]+1
+            else:
+                T[i][j] = max(T[i-1][j-1], T[i-1][j], T[i][j-1])
+    print(T)
+
+    return T[len(y)][len(x)]
+
+print(findLCS(x,y))
